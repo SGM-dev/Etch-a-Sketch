0b811485fa99cff81ba625ee45cc36d489a48103
Sam George Mathew <sam.mathew19113@gmail.com>
2022-08-09 12:12:16 +0530

    create function to generate random color

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ createGrid = () => {
     grid.appendChild(div);
   }
 };
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+};
+
+//gets random color
+function getRandomColor() {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
